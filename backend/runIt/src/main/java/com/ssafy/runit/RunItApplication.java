b2b31bf356d933eee9160d83d1ee21a5c3afcb71
jaehan4707 <jahan4707@naver.com>
2024-10-22 17:49:23 +0900

    chore(#S11P31D102-53): 프로젝트 초기 세팅

---
+++ backend/runIt/src/main/java/com/ssafy/runit/RunItApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.runit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunItApplication.class, args);
+    }
+
+}
