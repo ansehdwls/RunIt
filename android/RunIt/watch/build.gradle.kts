493fe59d6daeb5f03af23698c9f2c8cb64e005f9
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-10-22 18:01:00 +0900

    ui : watch 세팅

--- android/RunIt/watch/build.gradle.kts
+++ android/RunIt/watch/build.gradle.kts
@@ -4,15 +4,18 @@ plugins {
 }
 
 android {
-    namespace = "com.joku.watch"
+    namespace = "com.zoku.watch"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.joku.watch"
+        applicationId = "com.zoku.watch"
         minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
 
     }
 
@@ -35,20 +38,29 @@ android {
     buildFeatures {
         compose = true
     }
-
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
-
+    //androidx
     implementation(libs.play.services.wearable)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.tooling.preview)
-
     implementation(libs.wear.compose.material)
     implementation(libs.wear.compose.foundation)
-
-    implementation(libs.horologist.composables)
-    implementation(libs.horologist.compose.layout)
-    implementation(libs.horologist.compose.material)
+    implementation(libs.androidx.wear.tooling.preview)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.core.splashscreen)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
