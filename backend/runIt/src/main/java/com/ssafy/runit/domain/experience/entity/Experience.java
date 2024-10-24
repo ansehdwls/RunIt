a1d2f1c1a61480f7563010fcbfd9faa4b7185d57
최영호 <rhkeh123@naver.com>
2024-10-24 13:59:34 +0900

    feat (S11P31D102-85) : 경험치 및 유저 엔티티 생성

---
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/experience/entity/Experience.java
@@ -0,0 +1,31 @@
+package com.ssafy.runit.domain.experience.entity;
+
+import com.ssafy.runit.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+public class Experience {
+    @Id
+    @GeneratedValue
+    @Column(name = "experience_id")
+    private long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String activity;
+
+    private long changed;
+
+    private Timestamp createAt;
+
+    private Date startDate;
+
+}
