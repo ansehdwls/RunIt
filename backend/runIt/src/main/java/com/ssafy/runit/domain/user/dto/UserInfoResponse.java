fd83d45cc0b980854d56a0a2993ce799ef38029c
jaehan4707 <jahan4707@naver.com>
2024-10-27 00:24:24 +0900

    feat(#S11P31D102-60): UserInfoResponse 추가

---
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/user/dto/UserInfoResponse.java
@@ -0,0 +1,23 @@
+package com.ssafy.runit.domain.user.dto;
+
+import com.ssafy.runit.domain.user.entity.User;
+import lombok.Builder;
+
+@Builder
+public record UserInfoResponse(
+        long userId,
+        String userEmail,
+        String userName,
+        String imageUrl,
+        long groupId) {
+
+    public static UserInfoResponse fromEntity(User user) {
+        return UserInfoResponse.builder()
+                .userId(user.getId())
+                .userEmail(user.getUserEmail())
+                .userName(user.getUserName())
+                .imageUrl(user.getImageUrl())
+                .groupId(user.getUserGroup().getId())
+                .build();
+    }
+}
