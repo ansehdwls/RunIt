77a01551900ec265e456372c262b75583c8d8006
jaehan4707 <jahan4707@naver.com>
2024-10-26 20:36:13 +0900

    feat(#S11P31D102-58): ServerErrorCode 추가

---
+++ backend/runIt/src/main/java/com/ssafy/runit/exception/code/ServerErrorCode.java
@@ -0,0 +1,32 @@
+package com.ssafy.runit.exception.code;
+
+import com.ssafy.runit.exception.ErrorCodeType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ServerErrorCode implements ErrorCodeType {
+
+    UNKNOWN_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S-001", "서버 내부 오류입니다");
+
+    private final HttpStatus status;
+    private final String errorCode;
+    private final String message;
+
+    @Override
+    public HttpStatus httpStatus() {
+        return this.status;
+    }
+
+    @Override
+    public String message() {
+        return this.message;
+    }
+
+    @Override
+    public String errorCode() {
+        return this.errorCode;
+    }
+}
