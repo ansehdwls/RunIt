c231b8381613742308a6f4052cc2eba8bf6f0bad
mdj928 <moondj928@gmail.com>
2024-10-28 17:01:07 +0900

    refactor: Map, Graph 공통 Composable 사용

--- android/RunIt/feature/home/src/main/java/com/zoku/home/RunHistoryDetailScreen.kt
+++ android/RunIt/feature/home/src/main/java/com/zoku/home/RunHistoryDetailScreen.kt
@@ -1,16 +1,11 @@
 package com.zoku.home
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.zoku.ui.componenet.RecordMap
 
 @Composable
 fun RunHistoryDetailScreen() {
