5faeb833fdd8ae5bfe013724cba77b750466c422
jaehan4707 <jahan4707@naver.com>
2024-10-28 23:16:03 +0900

    feat(#S11P31D102-159): FcmToken 저장 API 추가

--- backend/runIt/src/main/java/com/ssafy/runit/domain/user/repository/UserRepository.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.ssafy.runit.domain.user.repository;
 import com.ssafy.runit.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +14,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUserEmail(String email);
 
     Optional<User> findByUserEmail(String email);
+
+    @Modifying
+    @Query("UPDATE User u SET u.fcmToken = :fcmToken WHERE u.id = :userId")
+    void updateFcmTokenByUserId(@Param("userId") Long userId, @Param("fcmToken") String fcmToken);
 
     @Query("SELECT u.fcmToken FROM User u WHERE u.fcmToken IS NOT NULL")
     List<String> findAllFcmTokens();
