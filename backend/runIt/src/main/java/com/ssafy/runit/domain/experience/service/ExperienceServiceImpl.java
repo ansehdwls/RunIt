15b4ba1c051e78912469f5936665494f8fe2942e
최영호 <rhkeh123@naver.com>
2024-10-28 13:56:45 +0900

    feat (S11P31D102-147) : 에러코드 exp -> auth로 수정

--- backend/runIt/src/main/java/com/ssafy/runit/domain/experience/service/ExperienceServiceImpl.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/experience/service/ExperienceServiceImpl.java
@@ -13,6 +13,7 @@ import java.time.LocalDate;
 import com.ssafy.runit.domain.user.entity.User;
 import com.ssafy.runit.domain.user.repository.UserRepository;
 import com.ssafy.runit.exception.CustomException;
+import com.ssafy.runit.exception.code.AuthErrorCode;
 import com.ssafy.runit.exception.code.ExperienceErrorCode;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
@@ -41,7 +42,7 @@ public class ExperienceServiceImpl implements ExperienceService {
     public void experienceSave(Long userId, ExperienceSaveRequest request) {
 
 //      기존의 user id 받아서 처리하는 부분
-        User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(ExperienceErrorCode.FIND_USER_FAIL));
+        User user = userRepository.findById(userId).orElseThrow(() -> new CustomException(AuthErrorCode.UNREGISTERED_USER_ERROR));
         Experience exp = request.Mapper(user);
         experienceRepository.save(exp);
     }
