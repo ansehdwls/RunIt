3a1f3765debae0b422153e7742b755215d0c62b4
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-10-28 10:30:37 +0900

    ui : Running UI 일단 완료

--- android/RunIt/watch/src/main/java/com/zoku/watch/screen/RunningScreen.kt
+++ android/RunIt/watch/src/main/java/com/zoku/watch/screen/RunningScreen.kt
@@ -1,6 +1,7 @@
 package com.zoku.watch.screen
 
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,22 +10,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material.icons.rounded.Stop
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.ButtonDefaults
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.OutlinedButton
+import androidx.wear.compose.material.Text
 import androidx.wear.tooling.preview.devices.WearDevices
 import com.zoku.ui.BaseYellow
+import com.zoku.watch.component.PagerScreen
 import com.zoku.watch.component.StatusText
 
 @Composable
@@ -32,6 +38,24 @@ fun RunningScreen(
     modifier: Modifier = Modifier
 ) {
     val scrollState = rememberScrollState()
+    val items: List<@Composable () -> Unit> =
+        listOf({
+            RunningStatus()
+        }, { RunningPause(scrollState = scrollState) })
+    val pagerState = rememberPagerState(pageCount = { items.size })
+    PagerScreen(
+        modifier = modifier,
+        state = pagerState,
+        items = items
+    )
+
+}
+
+@Composable
+fun RunningPause(
+    modifier: Modifier = Modifier,
+    scrollState: ScrollState
+) {
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -68,6 +92,37 @@ fun RunningScreen(
 
 
 @Composable
+fun RunningStatus(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(Modifier.height(50.dp))
+        Text(
+            modifier = Modifier,
+            text = "6.39",
+            fontSize = 50.sp,
+            color = BaseYellow
+        )
+        Text(
+            modifier = Modifier,
+            text = "KM",
+            fontSize = 20.sp,
+        )
+        Spacer(Modifier.height(20.dp))
+        Text(
+            modifier = Modifier,
+            text = "13:50",
+            fontSize = 20.sp,
+        )
+    }
+
+}
+
+
+@Composable
 fun RunningButton(
     modifier: Modifier = Modifier,
     icon: ImageVector
@@ -96,5 +151,11 @@ fun RunningButton(
 @Composable
 fun RunningPreview() {
     RunningScreen()
+}
+
+@Preview(device = WearDevices.LARGE_ROUND, showSystemUi = true, apiLevel = 33)
+@Composable
+fun RunningStatusPreview() {
+    RunningStatus()
 }
 
