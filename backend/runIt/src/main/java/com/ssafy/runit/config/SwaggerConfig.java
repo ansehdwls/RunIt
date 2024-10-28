ddf05bff8a69e227c668da1f7e8c57ed9742d5ef
jaehan4707 <jahan4707@naver.com>
2024-10-28 10:38:57 +0900

    feat(#S11P31D102-137): Swagger-Ui 인증 인가 처리 토큰 추가

--- backend/runIt/src/main/java/com/ssafy/runit/config/SwaggerConfig.java
+++ backend/runIt/src/main/java/com/ssafy/runit/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.ssafy.runit.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,11 +13,22 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openApi() {
-        Info info = new Info()
-                .version("1.0.0")
-                .title("RunIt Spring Server API Docs")
-                .description("RunIt Spring Server API");
-        return new OpenAPI().components(new Components())
-                .info(info);
+        return new OpenAPI()
+                .components(new Components())
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(
+                        new Info()
+                                .version("1.0.0")
+                                .title("RunIt Spring Server API Docs")
+                                .description("RunIt Spring Server API"));
+    }
+
+    public SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+                .scheme("bearer")
+                .bearerFormat("JWT");
     }
 }
