c29915f12b676be823f7bc55feff9313496da93e
jaehan4707 <jahan4707@naver.com>
2024-10-28 12:59:13 +0900

    feat(#S11P31D102-137): MethodArgumentNotValidException, ServerErrorCode 추가

--- backend/runIt/src/main/java/com/ssafy/runit/exception/GlobalExceptionHandler.java
+++ backend/runIt/src/main/java/com/ssafy/runit/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.ssafy.runit.exception;
 
 
+import com.ssafy.runit.exception.code.ServerErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +16,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         ErrorCodeType errorCode = e.getErrorCodeType();
+        return new ResponseEntity<>(ErrorResponse.error(errorCode), errorCode.httpStatus());
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        ErrorCodeType errorCode = ServerErrorCode.METHOD_ARGUMENT_ERROR;
         return new ResponseEntity<>(ErrorResponse.error(errorCode), errorCode.httpStatus());
     }
 }
