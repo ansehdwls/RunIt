d14920ce994dad14c3bccccaa434e6f2152015df
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-10-29 11:20:06 +0900

    feat: RunningResultScreen 화면 구현

--- android/RunIt/feature/running/src/main/java/com/zoku/running/RunningResultScreen.kt
+++ android/RunIt/feature/running/src/main/java/com/zoku/running/RunningResultScreen.kt
@@ -1,17 +1,75 @@
 package com.zoku.running
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.zoku.ui.BaseDarkBackground
+import com.zoku.ui.BaseYellow
+import com.zoku.ui.CustomTypo
+import com.zoku.ui.componenet.RecordDetailInfo
 
 @Composable
-fun RunningResultScreen() {
-    Column {
-        Text("텍스트 확인", color = Color.White)
+fun RunningResultScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(BaseDarkBackground)
+    ) {
+
+        Spacer(modifier = Modifier.weight(0.02f))
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.4f)
+        ) {
+            KakaoMapImage()
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.7f)
+        ) {
+            RecordDetailInfo(startDestination = 1)
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.1f),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(BaseYellow),
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier
+                    .fillMaxWidth(0.3f)
+            ) {
+                Text(
+                    text = "확인",
+                    style = CustomTypo().jalnan,
+                    color = Color.Black
+                )
+            }
+        }
     }
 }
+
 
 @Preview
 @Composable
