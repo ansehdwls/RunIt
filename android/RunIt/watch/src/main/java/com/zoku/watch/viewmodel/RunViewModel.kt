9553f6ed538202f0b51147af103c7da72d5cd431
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-10-30 11:03:23 +0900

    feat(#S11P31D102-109) : 운동 시작 프로세스

---
+++ android/RunIt/watch/src/main/java/com/zoku/watch/viewmodel/RunViewModel.kt
@@ -0,0 +1,14 @@
+package com.zoku.watch.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class RunViewModel @Inject constructor(
+
+) : ViewModel() {
+
+
+}
