3a489cc5432cfaba49fe32b9aaa0923da3b032fa
jaehan4707 <jahan4707@naver.com>
2024-10-31 13:06:22 +0900

    feat(#S11P31D102-142): DateUtils getDayNameInKorean 메서드 추가

--- backend/runIt/src/main/java/com/ssafy/runit/util/DateUtils.java
+++ backend/runIt/src/main/java/com/ssafy/runit/util/DateUtils.java
@@ -7,7 +7,18 @@ public class DateUtils {
 
     public static LocalDate getLastMonday() {
         LocalDate today = LocalDate.now();
-        LocalDate lastMonday = today.with(DayOfWeek.MONDAY);
-        return lastMonday.atStartOfDay();
+        return today.with(DayOfWeek.MONDAY);
+    }
+
+    public static String getDayNameInKorean(DayOfWeek day) {
+        return switch (day) {
+            case SUNDAY -> "일요일";
+            case MONDAY -> "월요일";
+            case TUESDAY -> "화요일";
+            case WEDNESDAY -> "수요일";
+            case THURSDAY -> "목요일";
+            case FRIDAY -> "금요일";
+            case SATURDAY -> "토요일";
+        };
     }
 }
