b39610ddaa25373a0c76cbcdad30e4d7be6c328b
jaehan4707 <jahan4707@naver.com>
2024-11-05 23:33:47 +0900

    feat(#S11P31D102-196): auth dto,entity 생성자 어노테이션 추가

--- backend/runIt/src/main/java/com/ssafy/runit/domain/auth/dto/request/UpdateJwtRequest.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/auth/dto/request/UpdateJwtRequest.java
@@ -1,9 +1,11 @@
 package com.ssafy.runit.domain.auth.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UpdateJwtRequest {
     @NotNull(message = "refreshToken is not null")
     private String refreshToken;
