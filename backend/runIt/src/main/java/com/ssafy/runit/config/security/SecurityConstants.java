7cd0155fd749e7b862cc89204d9f04fdfe0d29e8
jaehan4707 <jahan4707@naver.com>
2024-11-05 10:59:25 +0900

    feat(#S11P31D102-194): Cors allowed origin 수정

--- backend/runIt/src/main/java/com/ssafy/runit/config/security/SecurityConstants.java
+++ backend/runIt/src/main/java/com/ssafy/runit/config/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package com.ssafy.runit.config.security;
 
 public final class SecurityConstants {
-    public static final String[] CORS_ALLOWED_ORIGINS = {"*"};
+    public static final String[] CORS_ALLOWED_ORIGINS = {"http://3.35.169.202:8081"};
     public static final String[] CORS_ALLOWED_METHODS = {"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"};
     public static final String[] CORS_ALLOWED_HEADERS = {"*"};
     public static final String[] CORS_EXPOSE_HEADER = {"Authorization"};
