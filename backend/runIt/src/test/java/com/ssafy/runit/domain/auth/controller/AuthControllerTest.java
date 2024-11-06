097920433f3ed5d05f848aa40cff6aec008d0995
jaehan4707 <jahan4707@naver.com>
2024-11-06 14:48:51 +0900

    test(#S11P31D102-197): Jwt 토큰 갱신 성공 검증 테스트 코드 추가

--- backend/runIt/src/test/java/com/ssafy/runit/domain/auth/controller/AuthControllerTest.java
+++ backend/runIt/src/test/java/com/ssafy/runit/domain/auth/controller/AuthControllerTest.java
@@ -1,8 +1,10 @@
 package com.ssafy.runit.domain.auth.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.runit.domain.auth.dto.request.UpdateJwtRequest;
 import com.ssafy.runit.domain.auth.dto.request.UserLoginRequest;
 import com.ssafy.runit.domain.auth.dto.request.UserRegisterRequest;
+import com.ssafy.runit.domain.auth.dto.response.LoginResponse;
 import com.ssafy.runit.domain.auth.service.AuthService;
 import com.ssafy.runit.exception.CustomException;
 import com.ssafy.runit.exception.code.AuthErrorCode;
@@ -11,6 +13,7 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -37,6 +40,7 @@ public class AuthControllerTest {
 
     private static final String BASE_URL = "api/auth/";
     private static final String TEST_NUMBER = "1234";
+    private static final String REFRESH_TOKEN = "refreshToken";
 
     @BeforeEach
     void setPort() {
@@ -111,6 +115,9 @@ public class AuthControllerTest {
     @DisplayName("[로그인] - 성공 검증")
     void loginUser_Success() throws Exception {
         UserLoginRequest request = new UserLoginRequest(TEST_NUMBER);
+        LoginResponse response = new LoginResponse("newAccessToken", "newRefreshToken");
+        when(authService.login(ArgumentMatchers.any(UserLoginRequest.class)))
+                .thenReturn(response);
         given()
                 .port(port)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -119,7 +126,8 @@ public class AuthControllerTest {
                 .then().log().all()
                 .statusCode(200)
                 .body("message", equalTo("로그인에 성공했습니다"))
-                .body("data", nullValue());
+                .body("data.accessToken", equalTo(response.getAccessToken()))
+                .body("data.refreshToken", equalTo(response.getRefreshToken()));
     }
 
     @Test
@@ -152,5 +160,24 @@ public class AuthControllerTest {
                 .statusCode(400)
                 .body("message", equalTo(ServerErrorCode.METHOD_ARGUMENT_ERROR.message()))
                 .body("errorCode", equalTo(ServerErrorCode.METHOD_ARGUMENT_ERROR.errorCode()));
+    }
+
+    @Test
+    @DisplayName("[Jwt 토큰] - 갱신 성공 검증")
+    void updateJwtToken_Success() throws Exception {
+        UpdateJwtRequest request = new UpdateJwtRequest(TEST_NUMBER);
+        LoginResponse response = new LoginResponse("newAccessToken", "newRefreshToken");
+        when(authService.getNewRefreshToken(ArgumentMatchers.any(UpdateJwtRequest.class)))
+                .thenReturn(response);
+        given()
+                .port(port)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(objectMapper.writeValueAsString(request))
+                .when().post(BASE_URL + "token")
+                .then().log().all()
+                .statusCode(200)
+                .body("message", equalTo("JWT Token 갱신에 성공했습니다"))
+                .body("data.accessToken", equalTo(response.getAccessToken()))
+                .body("data.refreshToken", equalTo(response.getRefreshToken()));
     }
 }
