b37bece98235128e405044827270551a5db706f6
최영호 <rhkeh123@naver.com>
2024-11-06 16:45:51 +0900

    feat (S11P31D102-177) : track api serviceimpl 수정

--- backend/runIt/src/main/java/com/ssafy/runit/domain/track/service/TrackServiceImpl.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/track/service/TrackServiceImpl.java
@@ -2,6 +2,7 @@ package com.ssafy.runit.domain.track.service;
 
 import com.ssafy.runit.domain.track.dto.response.TrackImgResponse;
 import com.ssafy.runit.domain.track.dto.response.TrackRouteResponse;
+import com.ssafy.runit.domain.track.entity.Track;
 import com.ssafy.runit.domain.track.repository.TrackRepository;
 import com.ssafy.runit.exception.CustomException;
 import com.ssafy.runit.exception.code.TrackErrorCode;
@@ -19,15 +20,19 @@ public class TrackServiceImpl implements TrackService {
 
     @Override
     public TrackImgResponse getTrackImg(Long recordId) {
-        return trackRepository.findTrackImageUrlByRecordId(recordId).orElseThrow(
+        Track track = trackRepository.findTrackImageUrlByRecordId(recordId).orElseThrow(
                 () -> new CustomException(TrackErrorCode.NOT_FOUND_TRACK_IMG)
         );
+
+        return TrackImgResponse.fromEntity(track.getTrackImageUrl());
     }
 
     @Override
     public TrackRouteResponse getTrackRoute(Long recordId) {
-        return trackRepository.findTrackPathByRecordId(recordId).orElseThrow(
+        Track track = trackRepository.findTrackPathByRecordId(recordId).orElseThrow(
                 () -> new CustomException(TrackErrorCode.NOT_FOUND_TRACK_ROUTE)
         );
+
+        return TrackRouteResponse.fromEntity(track.getPath());
     }
 }
