4648ea8f3c6f3926398c5aa3b3caf1d92f1f4085
최영호 <rhkeh123@naver.com>
2024-11-06 16:45:26 +0900

    feat (S11P31D102-177) : track api response 수정

--- backend/runIt/src/main/java/com/ssafy/runit/domain/track/dto/response/TrackImgResponse.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/track/dto/response/TrackImgResponse.java
@@ -1,7 +1,14 @@
 package com.ssafy.runit.domain.track.dto.response;
 
+import lombok.Builder;
+
+@Builder
 public record TrackImgResponse(
     String trackImageUrl
 ) {
-
+    public static TrackImgResponse fromEntity(String url){
+        return TrackImgResponse.builder()
+                .trackImageUrl(url)
+                .build();
+    }
 }
