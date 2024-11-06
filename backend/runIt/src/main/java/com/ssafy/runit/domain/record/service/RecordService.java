19c45e41605ed8e879c966ae316143062784dfbe
최영호 <rhkeh123@naver.com>
2024-11-06 16:46:19 +0900

    feat (S11P31D102-177) : record api service 수정

--- backend/runIt/src/main/java/com/ssafy/runit/domain/record/service/RecordService.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/record/service/RecordService.java
@@ -1,17 +1,19 @@
 package com.ssafy.runit.domain.record.service;
 
 import com.ssafy.runit.domain.record.dto.request.RecordSaveRequest;
+import com.ssafy.runit.domain.record.dto.response.RecordGetListResponse;
 import com.ssafy.runit.domain.record.dto.response.RecordGetResponse;
-import com.ssafy.runit.domain.user.entity.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface RecordService {
-    void saveRunningRecord(User user, RecordSaveRequest request, MultipartFile file);
+    void saveRunningRecord(UserDetails userDetails, RecordSaveRequest request, MultipartFile file);
 
-    RecordGetResponse getRecord(Long userId, Long recordId);
+    RecordGetResponse getRecord(UserDetails userDetails, Long recordId);
 
-    List<RecordGetResponse> getRecordList(Long userId);
+    List<RecordGetListResponse> getRecordList(UserDetails userDetails);
 }
--- backend/runIt/src/main/java/com/ssafy/runit/domain/record/service/RecordServiceImpl.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/record/service/RecordServiceImpl.java
@@ -1,25 +1,27 @@
 package com.ssafy.runit.domain.record.service;
 
+import com.ssafy.runit.domain.pace.dto.response.PaceResponse;
 import com.ssafy.runit.domain.pace.entity.Pace;
 import com.ssafy.runit.domain.pace.repository.PaceRepository;
 import com.ssafy.runit.domain.record.dto.request.RecordSaveRequest;
+import com.ssafy.runit.domain.record.dto.response.RecordGetListResponse;
 import com.ssafy.runit.domain.record.dto.response.RecordGetResponse;
 import com.ssafy.runit.domain.record.entity.Record;
 import com.ssafy.runit.domain.record.repository.RecordRepository;
-import com.ssafy.runit.domain.track.dto.TrackSaveDto;
 import com.ssafy.runit.domain.track.entity.Track;
 import com.ssafy.runit.domain.track.repository.TrackRepository;
 import com.ssafy.runit.domain.user.entity.User;
 import com.ssafy.runit.domain.user.repository.UserRepository;
 import com.ssafy.runit.exception.CustomException;
-import com.ssafy.runit.exception.code.AuthErrorCode;
 import com.ssafy.runit.exception.code.RecordErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,8 +38,10 @@ public class RecordServiceImpl implements RecordService{
 
     @Override
     @Transactional
-    public void saveRunningRecord(User user, RecordSaveRequest request, MultipartFile file) {
-        Record record = request.mapper(user);
+    public void saveRunningRecord(UserDetails userDetails, RecordSaveRequest request, MultipartFile file) {
+        User findUser = userRepository.findByUserNumber(userDetails.getUsername()).orElseThrow();
+
+        Record record = request.mapper(findUser);
 
         recordRepository.save(record);
         Record afRecord = request.toEntity(record);
@@ -47,6 +51,7 @@ public class RecordServiceImpl implements RecordService{
         List<Pace> paceList = afRecord.getPaceList();
 
         for (Pace item : paceList){
+            log.debug("item = {} {}", item.getBpm(), item.getDuration());
             paceRepository.save(item);
         }
 
@@ -55,21 +60,40 @@ public class RecordServiceImpl implements RecordService{
     }
 
     @Override
-    public RecordGetResponse getRecord(Long userId, Long recordId) {
-        Record record = recordRepository.findByUserIdAndRecordId(userId, recordId).orElseThrow(
+    public RecordGetResponse getRecord(UserDetails userDetails, Long recordId) {
+        User findUser = userRepository.findByUserNumber(userDetails.getUsername()).orElseThrow();
+
+        log.debug("user = {}", findUser);
+
+        Record record = recordRepository.findByUserIdAndRecordId(findUser.getId(), recordId).orElseThrow(
                 () -> new CustomException(RecordErrorCode.NOT_FOUND_RECORD_DATA)
         );
 
-        User user = userRepository.findByUserNumber()
+        List<Pace> pace = paceRepository.findByRecordId(recordId);
+        List<PaceResponse> paceResponseList = new ArrayList<>();
+        for(Pace item : pace){
+            log.debug("item = {} {}", item.getDuration(), item.getBpm());
+            paceResponseList.add(PaceResponse.isEntity(item.getBpm(), item.getDuration()));
+        }
 
-        return RecordGetResponse.fromEntity(record,"" , "");
+        return RecordGetResponse.fromEntity(record, paceResponseList);
     }
 
     @Override
-    public List<RecordGetResponse> getRecordList(Long userId) {
+    public List<RecordGetListResponse> getRecordList(UserDetails userDetails) {
+        User findUser = userRepository.findByUserNumber(userDetails.getUsername()).orElseThrow();
+        List<Record> record = recordRepository.findByUserId(findUser.getId());
 
-        return recordRepository.findByUserId(userId).orElseThrow(
-                () -> new CustomException(RecordErrorCode.NOT_FOUND_RECORD_DATA_LIST)
-        );
+
+        List<RecordGetListResponse> recordGetListResponses = new ArrayList<>();
+
+        for(Record item : record){
+            Track track = trackRepository.findTrackImageUrlByRecordId(item.getId()).orElseThrow();
+            recordGetListResponses.add(RecordGetListResponse.fromEntity(item, findUser.getUserName(), track.getTrackImageUrl()));
+        }
+
+
+
+        return recordGetListResponses;
     }
 }
