a4904304e830a4871139a421ca5fa18bc8ecf06a
최영호 <rhkeh123@naver.com>
2024-11-06 16:45:37 +0900

    feat (S11P31D102-177) : track api repo 수정

--- backend/runIt/src/main/java/com/ssafy/runit/domain/track/repository/TrackRepository.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/track/repository/TrackRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TrackRepository extends JpaRepository<Track, Long> {
-    Optional<TrackImgResponse> findTrackImageUrlByRecordId(Long recordId);
+    Optional<Track> findTrackImageUrlByRecordId(Long recordId);
 
-    Optional<TrackRouteResponse> findTrackPathByRecordId(Long recordId);
+    Optional<Track> findTrackPathByRecordId(Long recordId);
 }
