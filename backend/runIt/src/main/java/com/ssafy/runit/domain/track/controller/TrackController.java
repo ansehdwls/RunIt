17e4f426a4d256d4c3dc5d81a08bf96b21be337f
최영호 <rhkeh123@naver.com>
2024-11-06 12:42:36 +0900

    feat (S11P31D102-177) : Track api controller 작성

--- backend/runIt/src/main/java/com/ssafy/runit/domain/track/controller/TrackController.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/track/controller/TrackController.java
@@ -5,20 +5,20 @@ import com.ssafy.runit.domain.record.dto.response.RecordGetResponse;
 import com.ssafy.runit.domain.track.dto.response.TrackImgResponse;
 import com.ssafy.runit.domain.track.dto.response.TrackRouteResponse;
 import com.ssafy.runit.domain.track.service.TrackService;
-import com.ssafy.runit.domain.user.entity.User;
 import com.ssafy.runit.domain.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+
 public class TrackController implements TrackDocs {
     private final UserRepository userRepository;
     private final TrackService trackService;
 
     @Override
+    @GetMapping()
     public RunItApiResponse<TrackImgResponse> trackFindImg(Long recordId) {
         TrackImgResponse image = trackService.getTrackImg(recordId);
         return new RunItApiResponse<>(image, "성공");
