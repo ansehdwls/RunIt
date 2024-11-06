63b560a698c53166a184ba6f48f005a1b935abfb
최영호 <rhkeh123@naver.com>
2024-11-06 13:32:28 +0900

    feat (S11P31D102-177) : Track controller path 설정

--- backend/runIt/src/main/java/com/ssafy/runit/domain/track/controller/TrackController.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/track/controller/TrackController.java
@@ -8,23 +8,25 @@ import com.ssafy.runit.domain.track.service.TrackService;
 import com.ssafy.runit.domain.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-
+@RequestMapping("/api/route/")
 public class TrackController implements TrackDocs {
     private final UserRepository userRepository;
     private final TrackService trackService;
 
     @Override
-    @GetMapping()
+    @GetMapping("img/{recordId}")
     public RunItApiResponse<TrackImgResponse> trackFindImg(Long recordId) {
         TrackImgResponse image = trackService.getTrackImg(recordId);
         return new RunItApiResponse<>(image, "성공");
     }
 
     @Override
+    @GetMapping("coordinate/{recordId}")
     public RunItApiResponse<TrackRouteResponse> trackFindRoute(Long recordId) {
         TrackRouteResponse route = trackService.getTrackRoute(recordId);
         return new RunItApiResponse<>(route, "성공");
