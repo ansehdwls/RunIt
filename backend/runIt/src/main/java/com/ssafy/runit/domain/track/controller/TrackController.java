6b5eed74d66bff4d9dd97c05c48fc9776f464caf
최영호 <rhkeh123@naver.com>
2024-11-06 11:26:17 +0900

    feat (S11P31D102-177) : Track api controller 및 docs 작성

--- backend/runIt/src/main/java/com/ssafy/runit/domain/track/controller/TrackController.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/track/controller/TrackController.java
@@ -1,4 +1,32 @@
 package com.ssafy.runit.domain.track.controller;
 
-public class TrackController {
+import com.ssafy.runit.RunItApiResponse;
+import com.ssafy.runit.domain.record.dto.response.RecordGetResponse;
+import com.ssafy.runit.domain.track.dto.response.TrackImgResponse;
+import com.ssafy.runit.domain.track.dto.response.TrackRouteResponse;
+import com.ssafy.runit.domain.track.service.TrackService;
+import com.ssafy.runit.domain.user.entity.User;
+import com.ssafy.runit.domain.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class TrackController implements TrackDocs {
+    private final UserRepository userRepository;
+    private final TrackService trackService;
+
+    @Override
+    public RunItApiResponse<TrackImgResponse> trackFindImg(Long recordId) {
+        TrackImgResponse image = trackService.getTrackImg(recordId);
+        return new RunItApiResponse<>(image, "성공");
+    }
+
+    @Override
+    public RunItApiResponse<TrackRouteResponse> trackFindRoute(Long recordId) {
+        TrackRouteResponse route = trackService.getTrackRoute(recordId);
+        return new RunItApiResponse<>(route, "성공");
+    }
 }
