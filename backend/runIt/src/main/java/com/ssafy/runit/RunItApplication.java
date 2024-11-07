1200af14c873392a02e9f7fffaf82ad7eedbf0e0
jaehan4707 <jahan4707@naver.com>
2024-11-08 01:48:20 +0900

    feat(#S11P31D102-215): @EnableCaching 추가

--- backend/runIt/src/main/java/com/ssafy/runit/RunItApplication.java
+++ backend/runIt/src/main/java/com/ssafy/runit/RunItApplication.java
@@ -3,6 +3,7 @@ package com.ssafy.runit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableScheduling
 @EnableJpaAuditing
+@EnableCaching
 public class RunItApplication {
 
     public static void main(String[] args) {
