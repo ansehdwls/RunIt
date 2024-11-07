f179f0639ae6a41b94b2a3faea55998c54e64165
최영호 <rhkeh123@naver.com>
2024-11-07 15:39:16 +0900

    feat (S11P31D102-183) : S3 세팅

--- backend/runIt/src/main/java/com/ssafy/runit/util/S3UploadUtil.java
+++ backend/runIt/src/main/java/com/ssafy/runit/util/S3UploadUtil.java
@@ -1,20 +1,20 @@
-package com.ssafy.runit.domain.s3.service;
+package com.ssafy.runit.util;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@Service
+@Component
 @RequiredArgsConstructor
-public class S3UploadService {
+public class S3UploadUtil {
     private final AmazonS3 amazonS3;
 
-    @Value("${cloud.aws.credentials.bucket}")
+    @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
     public String saveFile(MultipartFile multipartFile) throws IOException {
