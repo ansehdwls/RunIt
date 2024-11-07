58378309cb0474adeab2c677e09c83de2e0a4af7
최영호 <rhkeh123@naver.com>
2024-11-07 11:26:04 +0900

    feat (S11P31D102-179) : s3 기본 세팅

---
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/s3/service/S3UploadService.java
@@ -0,0 +1,30 @@
+package com.ssafy.runit.domain.s3.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class S3UploadService {
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.credentials.bucket}")
+    private String bucket;
+
+    public String saveFile(MultipartFile multipartFile) throws IOException {
+        String originalFilename = multipartFile.getOriginalFilename();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+
+        amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), metadata);
+        return amazonS3.getUrl(bucket, originalFilename).toString();
+    }
+}
