92627dd4fed7a7a0cf650b6aa2d542338beb726f
jaehan4707 <jahan4707@naver.com>
2024-11-07 12:50:16 +0900

    feat(#S11P31D102-200): GetGroupUsersResponse class로 변경

--- backend/runIt/src/main/java/com/ssafy/runit/domain/group/dto/response/GetGroupUsersResponse.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/group/dto/response/GetGroupUsersResponse.java
@@ -1,14 +1,19 @@
 package com.ssafy.runit.domain.group.dto.response;
 
 import com.ssafy.runit.domain.user.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
-public record GetGroupUsersResponse(
-        String userName,
-        String imageUrl,
-        Long exp
-) {
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetGroupUsersResponse {
+    private String userName;
+    private String imageUrl;
+    private Long exp;
 
     public static GetGroupUsersResponse fromEntity(User user, Long exp) {
         return GetGroupUsersResponse.builder()
