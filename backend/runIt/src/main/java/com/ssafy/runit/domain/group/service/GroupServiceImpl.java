42bbd74ebc6b535b080a8bfa9a424613dac6103e
jaehan4707 <jahan4707@naver.com>
2024-11-07 12:53:35 +0900

    feat(#S11P31D102-200): 그룹 내 유저 랭킹 조회 개선

--- backend/runIt/src/main/java/com/ssafy/runit/domain/group/service/GroupServiceImpl.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/group/service/GroupServiceImpl.java
@@ -8,14 +8,12 @@ import com.ssafy.runit.domain.user.entity.User;
 import com.ssafy.runit.exception.CustomException;
 import com.ssafy.runit.exception.code.GroupErrorCode;
 import com.ssafy.runit.util.DateUtils;
-import com.ssafy.runit.util.ExperienceUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -32,13 +30,6 @@ public class GroupServiceImpl implements GroupService {
         );
         LocalDate lastMonday = DateUtils.getLastMonday();
         List<Long> userIds = group.getUsers().stream().map(User::getId).toList();
-        List<Long[]> experienceSums = experienceRepository.sumExperienceByUserIdsAndStartDate(userIds, lastMonday.atStartOfDay());
-        Map<Long, Long> map = ExperienceUtil.getGroupUserExperience(userIds, experienceSums);
-        return group.getUsers()
-                .stream()
-                .map(user ->
-                        GetGroupUsersResponse
-                                .fromEntity(user, map.getOrDefault(user.getId(), 0L)))
-                .toList();
+        return experienceRepository.sumExperienceByUserIdsAndStartDate(userIds, lastMonday.atStartOfDay());
     }
 }
