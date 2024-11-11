4e801da9634ff623e7c9f2971ad50cfccf4e8b77
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-11-11 12:48:42 +0900

    feat: 화면 진입 시 상태 저장

--- android/RunIt/feature/running/src/main/java/com/zoku/running/RunningResultScreen.kt
+++ android/RunIt/feature/running/src/main/java/com/zoku/running/RunningResultScreen.kt
@@ -1,17 +1,22 @@
 package com.zoku.running
 
 import android.os.Build
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -25,14 +30,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.zoku.ui.BaseDarkBackground
 import com.zoku.ui.BaseYellow
 import com.zoku.ui.CustomTypo
 import com.zoku.ui.componenet.KakaoMapView
 import com.zoku.ui.componenet.RecordDetailInfo
-import java.io.File
 
+@OptIn(ExperimentalMaterial3Api::class)
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun RunningResultScreen(
@@ -42,8 +48,8 @@ fun RunningResultScreen(
 ) {
     val context = LocalContext.current
     val totalRunningList by runningViewModel.totalRunningList.collectAsState()
-    var captureFile by remember { mutableStateOf<File?>(null) }
     var isMapCompleted by remember { mutableStateOf(false) }
+    var showDialog by remember { mutableStateOf(false) }
 
     Column(
         modifier = modifier
@@ -60,12 +66,23 @@ fun RunningResultScreen(
         ) {
             KakaoMapView(
                 totalLocationList = totalRunningList,
-                isResult = true,
                 onCaptureComplete = { file ->
-                    captureFile = file
+                    //TODO 이거 테스트용임 지워야함 , 누가 이 주석을 본다면 지우라고 해주세요;;
                     isMapCompleted = true
+//                    runningViewModel.postRunningRecord(
+//                        captureFile = file,
+//                        onSuccess = {
+//                            Toast.makeText(context, "통신 성공", Toast.LENGTH_SHORT).show()
+//                            isMapCompleted = true
+//                        },
+//                        onFail = { message ->
+//                            Toast.makeText(context, "API 실패 ${message}", Toast.LENGTH_SHORT)
+//                                .show()
+//                        }
+//                    )
                 },
-                initialLocation = runningViewModel.getInitialLocationData()
+                initialLocation = runningViewModel.getInitialLocationData(),
+                isResult = true
             )
         }
 
@@ -86,21 +103,8 @@ fun RunningResultScreen(
         ) {
             Button(
                 onClick = {
-                    captureFile?.let { file ->
-                        runningViewModel.postRunningRecord(
-                            captureFile = file,
-                            onSuccess = {
-                                Toast.makeText(context, "통신 성공", Toast.LENGTH_SHORT).show()
-                                moveToHome()
-                            },
-                            onFail = { message ->
-                                Toast.makeText(context, "API 실패 ${message}", Toast.LENGTH_SHORT)
-                                    .show()
-                            }
-                        )
-                    }
+                    showDialog = true
                 },
-
                 colors = ButtonDefaults.buttonColors(BaseYellow),
                 shape = RoundedCornerShape(8.dp),
                 modifier = Modifier
@@ -114,6 +118,87 @@ fun RunningResultScreen(
                 )
             }
         }
+    }
+
+    if (showDialog) {
+        BasicAlertDialog(onDismissRequest = { showDialog = false }) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable {
+                        showDialog = false
+                    },
+                verticalArrangement = Arrangement.Center
+            ) {
+                Spacer(modifier = Modifier.weight(0.4f))
+                Surface(modifier = Modifier.weight(0.2f)) {
+                    Text(
+                        text = "경로를 저장하시겠습니까?",
+                        style = CustomTypo().jalnan,
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 16.dp)
+                    )
+                }
+            }
+        }
+//        AlertDialog(
+//            onDismissRequest = { showDialog = false },
+//            shape = RoundedCornerShape(16.dp),
+//            containerColor = BaseDarkBackground,
+//            text = {
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxWidth(),
+//                    contentAlignment = Alignment.Center,
+//                ) {
+//                    Text(
+//                        text = "경로를 저장하시겠습니까?",
+//                        style = CustomTypo().jalnan,
+//                        fontSize = 16.sp,
+//                        color = Color.White,
+//                        modifier = Modifier.padding(top = 16.dp)
+//                    )
+//                }
+//            },
+//            confirmButton = {
+//                Row(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .padding(8.dp),
+//                    horizontalArrangement = Arrangement.SpaceEvenly
+//                ) {
+//                    TextButton(
+//                        onClick = { showDialog = false },
+//                        modifier = Modifier
+//                            .weight(1f)
+//                            .border(1.dp, Color.Gray, RoundedCornerShape(8.dp))
+//                    ) {
+//                        Text(
+//                            text = "취소",
+//                            style = CustomTypo().mapleLight,
+//                            color = Color.White
+//                        )
+//                    }
+//                    Spacer(modifier = Modifier.width(8.dp))
+//                    TextButton(
+//                        onClick = {
+//                            showDialog = false
+//                            moveToHome()
+//                        },
+//                        modifier = Modifier
+//                            .weight(1f)
+//                            .border(1.dp, Color.Gray, RoundedCornerShape(8.dp))
+//                    ) {
+//                        Text(
+//                            text = "확인",
+//                            style = CustomTypo().mapleLight,
+//                            color = BaseYellow
+//                        )
+//                    }
+//                }
+//            }
+//        )
     }
 }
 
