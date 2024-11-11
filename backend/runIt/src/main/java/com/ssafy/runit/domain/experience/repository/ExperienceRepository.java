0adb872821a0f0f895c1edbb9ea6d2615171d96c
jaehan4707 <jahan4707@naver.com>
2024-11-11 12:46:52 +0900

    feat(#S11P31D102-248): ExperienceRepository sumExperienceByUserIdsAndStartDate 제거, 경험치 추가 시 redis update 추가

--- backend/runIt/src/main/java/com/ssafy/runit/domain/experience/repository/ExperienceRepository.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/experience/repository/ExperienceRepository.java
@@ -2,7 +2,6 @@ package com.ssafy.runit.domain.experience.repository;
 
 import com.ssafy.runit.domain.experience.dto.response.ExperienceGetListResponse;
 import com.ssafy.runit.domain.experience.entity.Experience;
-import com.ssafy.runit.domain.group.dto.response.GroupUserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,12 +16,4 @@ public interface ExperienceRepository extends JpaRepository<Experience, Long> {
     Long experienceChangedSum(@Param("userId") Long userId, @Param("startDate") LocalDateTime startDate);
 
     List<ExperienceGetListResponse> findByUser_Id(Long userId);
-
-    @Query("SELECT new com.ssafy.runit.domain.group.dto.response.GroupUserInfo(u.userName, u.imageUrl, COALESCE(CAST(SUM(e.changed) AS long), 0L)) " +
-            "FROM User u LEFT JOIN u.experiences e " +
-            "ON e.createAt >= :startDate " +
-            "WHERE u.id IN :userIds " +
-            "GROUP BY u.id " +
-            "ORDER BY COALESCE(SUM(e.changed), 0) DESC")
-    List<GroupUserInfo> sumExperienceByUserIdsAndStartDate(@Param("userIds") List<Long> userIds, @Param("startDate") LocalDateTime startDate);
 }
--- backend/runIt/src/main/java/com/ssafy/runit/domain/experience/service/ExperienceServiceImpl.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/experience/service/ExperienceServiceImpl.java
@@ -5,17 +5,13 @@ import com.ssafy.runit.domain.experience.dto.request.ExperienceSaveRequest;
 import com.ssafy.runit.domain.experience.dto.response.ExperienceGetListResponse;
 import com.ssafy.runit.domain.experience.entity.Experience;
 import com.ssafy.runit.domain.experience.repository.ExperienceRepository;
+import com.ssafy.runit.domain.rank.service.RankService;
 import com.ssafy.runit.domain.user.entity.User;
-import com.ssafy.runit.domain.user.repository.UserRepository;
-import com.ssafy.runit.exception.CustomException;
-import com.ssafy.runit.exception.code.AuthErrorCode;
 import com.ssafy.runit.util.DateUtils;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -28,6 +24,7 @@ import java.util.List;
 public class ExperienceServiceImpl implements ExperienceService {
 
     private final ExperienceRepository experienceRepository;
+    private final RankService rankService;
 
 
     @Override
@@ -35,6 +32,7 @@ public class ExperienceServiceImpl implements ExperienceService {
     public void experienceSave(User user, ExperienceSaveRequest request) {
 //      기존의 user id 받아서 처리하는 부분
         Experience exp = request.Mapper(user, LocalDateTime.now());
+        rankService.updateScore(user.getUserGroup().getId(),user.getId(),request.getChanged());
         experienceRepository.save(exp);
     }
 
