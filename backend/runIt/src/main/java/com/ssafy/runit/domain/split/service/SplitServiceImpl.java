3988e46b9d723799d8047947f196761a6f571fc4
jaehan4707 <jahan4707@naver.com>
2024-11-13 16:17:29 +0900

    feat(#S11P31D102-252): PaceRankManager 추가

--- backend/runIt/src/main/java/com/ssafy/runit/domain/split/service/SplitServiceImpl.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/split/service/SplitServiceImpl.java
@@ -1,17 +1,11 @@
 package com.ssafy.runit.domain.split.service;
 
-import com.ssafy.runit.config.redis.RedisKeys;
+import com.ssafy.runit.domain.rank.service.PaceRankManager;
 import com.ssafy.runit.domain.record.entity.Record;
 import com.ssafy.runit.domain.split.repository.SplitRepository;
-import com.ssafy.runit.exception.CustomException;
-import com.ssafy.runit.exception.code.ServerErrorCode;
-import com.ssafy.runit.exception.code.SplitErrorCode;
-import com.ssafy.runit.util.DateUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class SplitServiceImpl implements SplitService {
 
     private final SplitRepository splitRepository;
+    private final PaceRankManager paceRankManager;
     private final RedisTemplate<String, Object> redisTemplate;
 
     @Transactional
@@ -29,39 +24,7 @@ public class SplitServiceImpl implements SplitService {
         String groupId = String.valueOf(record.getUser().getUserGroup().getId());
         String userId = String.valueOf(record.getUser().getId());
         splitRepository.saveAll(record.getSplitList());
-
-        savePace(record, groupId, userId);
-
+        paceRankManager.savePace(record, groupId, userId);
         //TODO 거리 저장 로직 추가
-    }
-
-    private void savePace(Record record, String groupId, String userId) {
-        String paceRankKey = RedisKeys.getPaceRankKey(groupId);
-        String paceHashKey = RedisKeys.getUserPaceKey(groupId);
-        Double averagePace = record.getPace();
-
-        if (averagePace == null) {
-            throw new CustomException(SplitErrorCode.PACE_CALCULATION_ERROR);
-        }
-
-        ZSetOperations<String, Object> zSet = redisTemplate.opsForZSet();
-        HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
-
-        hashOperations.increment(paceHashKey, RedisKeys.USER_PACE_SUB_KEY_TOTAL_PACE, averagePace);
-        hashOperations.increment(paceHashKey, RedisKeys.USER_PACE_SUB_KEY_COUNT, 1);
-
-        Double totalPace = (Double) hashOperations.get(paceHashKey, RedisKeys.USER_PACE_SUB_KEY_TOTAL_PACE);
-        Integer totalCount = (Integer) hashOperations.get(paceHashKey, RedisKeys.USER_PACE_SUB_KEY_COUNT);
-
-        if (totalCount == null || totalPace == null || totalCount == 0) {
-            throw new CustomException(ServerErrorCode.REDIS_RETRIEVAL_ERROR);
-        }
-
-        double newPace = totalPace / totalCount;
-        newPace = Math.round(newPace * 100.0) / 100.0;
-
-        zSet.add(paceRankKey, userId, newPace);
-        redisTemplate.expire(paceRankKey, DateUtils.computeDurationForNextWeek());
-        redisTemplate.expire(paceHashKey, DateUtils.computeDurationForNextWeek());
     }
 }
