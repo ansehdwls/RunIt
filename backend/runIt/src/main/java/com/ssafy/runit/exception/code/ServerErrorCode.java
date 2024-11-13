988620cdd677c1b521fa17dca46bec2686ed7ef0
jaehan4707 <jahan4707@naver.com>
2024-11-13 10:55:38 +0900

    feat(#S11P31D102-251): ServerErrorCode Redis 에러 추가

--- backend/runIt/src/main/java/com/ssafy/runit/exception/code/ServerErrorCode.java
+++ backend/runIt/src/main/java/com/ssafy/runit/exception/code/ServerErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum ServerErrorCode implements ErrorCodeType {
 
     UNKNOWN_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S-001", "서버 내부 오류입니다"),
-    METHOD_ARGUMENT_ERROR(HttpStatus.BAD_REQUEST, "S-002", "HTTP 요청이 잘못되었습니다.");
+    METHOD_ARGUMENT_ERROR(HttpStatus.BAD_REQUEST, "S-002", "HTTP 요청이 잘못되었습니다."),
+    REDIS_RETRIEVAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S-003", "레디스 내부 오류입니다.");
 
     private final HttpStatus status;
     private final String errorCode;
