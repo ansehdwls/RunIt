0571fb0b8cf3b0dc44a8279262396236fe55fbf3
최영호 <rhkeh123@naver.com>
2024-11-13 10:01:37 +0900

    feat (S11P31D102-232) : 타입 변경 및 파라미터 삭제

--- backend/runIt/src/main/java/com/ssafy/runit/domain/record/dto/RecordSaveDto.java
+++ backend/runIt/src/main/java/com/ssafy/runit/domain/record/dto/RecordSaveDto.java
@@ -12,11 +12,10 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RecordSaveDto {
-    private double distance;
+    private Integer distance;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private Integer bpm;
-    private Boolean isPractice;
 
     public Record toEntity(User user){
         return Record.builder()
