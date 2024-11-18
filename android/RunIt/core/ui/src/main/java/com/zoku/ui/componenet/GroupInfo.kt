b51ac1bf614b14ad27c4857dc78dae827231807f
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-11-18 13:38:51 +0900

    fix(#S11P31D102-225) : 그룹 정보 수정

--- android/RunIt/core/ui/src/main/java/com/zoku/ui/componenet/GroupInfo.kt
+++ android/RunIt/core/ui/src/main/java/com/zoku/ui/componenet/GroupInfo.kt
@@ -1,20 +1,18 @@
 package com.zoku.ui.componenet
 
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,13 +23,14 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.zoku.ui.R
 import com.zoku.ui.model.GroupInfoData
 import com.zoku.ui.theme.Black
 import com.zoku.ui.theme.CustomTypo
 
 @Composable
 fun ShowGroupInfo() {
+
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -39,37 +38,44 @@ fun ShowGroupInfo() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-            ,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            item {
-                Text(
-                    modifier = Modifier.padding(top = 30.dp),
-                    text = "리그 정보",
-                    style = CustomTypo().mapleBold.copy(
-                        fontSize = 30.sp,
-                        color = Color.White,
-                        textAlign = TextAlign.Center
+        Row {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                item {
+                    Text(
+                        modifier = Modifier.padding(top = 30.dp),
+                        text = "리그 정보",
+                        style = CustomTypo().mapleBold.copy(
+                            fontSize = 30.sp,
+                            color = Color.White,
+                            textAlign = TextAlign.Center
+                        )
                     )
-                )
-            }
-            items(GroupInfoData.DEFAULT) { data ->
-                Spacer(modifier = Modifier.height(20.dp))
-                ShowGroupItem(image = data.image, text = data.name)
+                }
+                itemsIndexed(GroupInfoData.DEFAULT) { index, data ->
+                    Spacer(modifier = Modifier.height(20.dp))
+                    ShowGroupItem(
+                        image = data.image,
+                        text = data.name,
+                        index = (index + 1).toString()
+                    )
+                }
             }
         }
+
     }
+
 }
 
 @Composable
 fun ShowGroupItem(
     modifier: Modifier = Modifier.wrapContentWidth(),
     image: Int,
-    text: String
+    text: String,
+    index: String
 ) {
     Row(
         modifier = modifier,
@@ -81,14 +87,30 @@ fun ShowGroupItem(
         )
         Spacer(modifier = Modifier.width(100.dp))
 
-        Text(
-            modifier = Modifier,
-            text = text,
-            style = CustomTypo().mapleLight.copy(
-                color = Color.White,
-                fontSize = 20.sp
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier,
+                text = "$index 단계",
+                style = CustomTypo().mapleLight.copy(
+                    color = Color.White,
+                    fontSize = 15.sp
+                )
             )
-        )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                modifier = Modifier,
+                text = text,
+                style = CustomTypo().mapleLight.copy(
+                    color = Color.White,
+                    fontSize = 20.sp
+                )
+            )
+
+        }
+
+
     }
 }
 
